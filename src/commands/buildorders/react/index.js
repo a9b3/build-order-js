a6934d41f4a0badb9c249305549459ff490db246
Sam Lau <esayemm@gmail.com>
2017-06-07 13:43:02 -0700

    react run scss watch in parallel

--- src/commands/buildorders/react/index.js
+++ src/commands/buildorders/react/index.js
@@ -29,7 +29,7 @@ export default async function react(opts) {
         'watch-scss': "./node_modules/chokidar-cli/index.js 'src/**/*.scss' -c 'touch src/app/index.js'",
         // docker will run 'npm run build'
         build: 'npm run webpack',
-        start: 'npm run webpack:dev && npm run watch-scss',
+        start: 'npm run webpack:dev & npm run watch-scss',
         deploy: 'npm run build && echo add continuous deployment here',
       },
     },
