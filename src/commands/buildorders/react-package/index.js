06101040fc5bef32a3e6a2710cc3a081b742688a
Sam Lau <esayemm@gmail.com>
2017-07-10 19:07:47 -0700

    node paths for react-package examples

---
+++ src/commands/buildorders/react-package/index.js
@@ -0,0 +1,57 @@
+import path       from 'path'
+import * as tasks from '../../tasks'
+import taskApi    from 'services/task-api'
+
+export default async function reactPackage({
+  flags,
+}) {
+  await tasks.bootstrap({ name: flags.name })
+  await tasks.eslint({ extend: 'eslint-config-esayemm/lib/react' })
+
+  await taskApi.addPackages({
+    packages: [
+      'jbs-fe',
+      'react-addons-test-utils',
+      'react',
+      'prop-types',
+      'react-css-modules',
+      'react-dom',
+      'react-helmet',
+      'react-router',
+      'react-router-dom',
+      'history',
+      'html',
+      'invariant',
+      'esayemm-styles',
+    ],
+    dev: true,
+  })
+
+  await taskApi.addToPackageJson({
+    json: {
+      main: `./build/index.js`,
+      module: `./build/index.es.js`,
+      scripts: {
+        build: 'BABEL_REACT=true NODE_ENV=prod NODE_PATH=./src ./node_modules/jbs-fe/bin.js build:package --input src --output build --es-input-file src/index.js --es-output-file build/index.es.js',
+        start: 'BABEL_REACT=true NODE_PATH=./example:./example/app:./src ./node_modules/jbs-fe/bin.js dev --app-index ./example/app/index.js --html-index ./example/index.html --context ./example',
+        test: `BABEL_REACT=true NODE_ENV=test ./node_modules/jbs-fe/bin.js test --single-run`,
+        'test:watch': `BABEL_REACT=true NODE_ENV=test ./node_modules/jbs-fe/bin.js test`,
+      },
+      babel: {
+        presets: ['./node_modules/jbs-fe/configs/babel-preset-jbs-fe.js'],
+      },
+    },
+  })
+
+  await taskApi.shell({ command: `mkdir src` })
+  await taskApi.shell({ command: `touch src/index.js` })
+
+  await taskApi.copyDirectory({
+    src: path.resolve(__dirname, './templates/src'),
+    dest: './example',
+  })
+
+  if (flags.git) {
+    await taskApi.gitInit()
+  }
+}
