480eff10876c4de9cb75d7db8a6a55e0973b8127
Sam Lau <esayemm@gmail.com>
2018-02-05 00:27:28 -0800

    update storybook configs

--- src/buildorders/tasks/storybook/templates/config.js
+++ src/buildorders/tasks/storybook/templates/config.js
@@ -1,8 +1,6 @@
 import { configure } from '@storybook/react'
 
 // import global styles here
-// import '../example/styles/index.scss'
-// import '../src/variables.css'
 
 function loadStories() {
   const req = require.context('../src', true, /\.stories\.js$/)
